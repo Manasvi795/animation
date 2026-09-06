c08b72185fd6a0915a4b13f89dba55135555b24d
Manasvi Kapoor <kapoormanasvi795@gmail.com>
2026-09-06 13:34:19 +0530

    Formatting

--- main.js
+++ main.js
@@ -87,12 +87,7 @@ function updateFruit() {
       lives--;
       if (lives <= 0) {
         gameOver = true;
-        showGameCard(
-          "GAME OVER",
-          "Your final score",
-          "PLAY AGAIN",
-          "SCORE: " + score,
-        );
+        showGameCard("GAME OVER", "PLAY AGAIN", "SCORE: " + score);
       }
     }
   }
@@ -187,8 +182,4 @@ function startGame() {
 cardButton.addEventListener("click", () => {
   startGame();
 });
-showGameCard(
-  "FRUIT SLICER",
-  "Slice the fruits and avoid the bombs!",
-  "START GAME",
-);
+showGameCard("FRUIT SLICER", "Pop the fruits", "START GAME");
